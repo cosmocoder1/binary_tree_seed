d07c135e8d0ce8b659736c7699f55792c01c7b1f
Nathan Lucy <cosmocoder1@gmail.com>
2021-02-24 19:06:23 -0600

    in prog

--- binary_tree_seed.rb
+++ binary_tree_seed.rb
@@ -1,25 +1,33 @@
-def binary_tree_seed (arr)
-  if arr.length === 1
-    arr
-  end 
-  
-  sort(arr)
-  
+class Node_Sprout
+  attr_accessor :left_child, :right_child
+  def initialize (data)
+    @data = data
+    @left_child = nil
+    @right_child = nil
+  end    
 end  
 
-def sort (arr)
-  if arr.length === 1
-    return arr
-  end  
+class Binary_Tree 
 
-  middle = (arr.length / 2) - 1
-  left = sort(arr[0..middle])
-  puts left
-  right = sort(arr[middle + 1..arr.length - 1])
-  puts right
-  
+  def sort (arr)
+
+    if arr.length === 1
+      return arr
+    end  
+   
+    middle = (arr.length / 2) - 1
+    left = arr[0..middle]
+    right = arr[middle + 1..arr.length - 1]
+    node = Node_Sprout.new(arr[middle])
+    node.left__child = sort(left)
+    node.right_child = sort(right) 
+
+  end
+
 end  
 
 arr_example = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-binary_tree_seed (arr_example)
+tree_test = Binary_Tree.new
+tree_test.sort(arr_example)
+
