eca4dccb5ee4395cd70813b641a4f69e0b5556e1
Nathan Lucy <cosmocoder1@gmail.com>
2021-02-26 10:51:16 -0600

    rebuild

--- binary_tree_seed.rb
+++ binary_tree_seed.rb
@@ -1,5 +1,10 @@
-class Node_Sprout
-  attr_accessor :left_child, :right_child
+class Node
+  include Comparable
+  attr_accessor :data, :left_child, :right_child
+  def <=>(other_data) 
+        data <=> other_data 
+  end
+
   def initialize (data)
     @data = data
     @left_child = nil
@@ -7,20 +12,37 @@ class Node_Sprout
   end    
 end  
 
-class Binary_Tree 
+class Tree 
 
-  def sort (arr)
+  def initialize (arr)
+    @arr = arr 
+  end  
+
+  def root (build tree)
+   
+  end
+  
+  def build_tree (arr)
+
+    
 
     if arr.length === 1
       return arr
     end  
-   
+
     middle = (arr.length / 2) - 1
     left = arr[0..middle]
     right = arr[middle + 1..arr.length - 1]
-    node = Node_Sprout.new(arr[middle])
-    node.left__child = sort(left)
-    node.right_child = sort(right) 
+
+    node = Node.new(arr[middle])
+    puts node.data
+    if left.length > 1
+      node.left__child = root(left)
+    elsif right.length > 1
+      node.right_child = root(right) 
+    else
+      return
+    end  
 
   end
 
@@ -28,6 +50,6 @@ end
 
 arr_example = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-tree_test = Binary_Tree.new
+tree_test = Tree.new
 tree_test.sort(arr_example)
 
